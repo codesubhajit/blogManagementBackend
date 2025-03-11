6a1ac3d4000b14b409e34a23aba670c7920687fb
Subhajit Ghosh <infosubhajit99@gmail.com>
2025-03-11 17:04:17 +0530

    frontend

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://blog-management-frontend-psi.vercel.app/",
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
